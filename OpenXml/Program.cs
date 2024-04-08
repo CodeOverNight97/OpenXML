5d28ea66ff394c65067a2549b01e5204565b3e0b
hoangson2002 <hoangson7112002@gmail.com>
2024-04-08 14:25:00 +0700

    sonht commit

--- OpenXml/Program.cs
+++ OpenXml/Program.cs
@@ -10,9 +10,27 @@ namespace OpenXml
         public static void Main(String[] args)
         {
             //var a  = int.Parse(Console.ReadLine());
+            //IFuntionCustom Fun = new FuntionCustom();
+            //var a = Fun.DocDuLieuExcel<TestDocDuLieuExcelmodel>("C:/LamViec/OpenXml/OpenXml/File/2.xlsx");
+            //Console.WriteLine(a);
+
+            var a = int.Parse(Console.ReadLine());
             IFuntionCustom Fun = new FuntionCustom();
-            var a = Fun.DocDuLieuExcel<TestDocDuLieuExcelmodel>("C:/LamViec/OpenXml/OpenXml/File/2.xlsx");
-            Console.WriteLine(a);
+            switch (a)
+            {
+                case 1:
+                    Fun.DocDuLieuExcel<dynamic>("");
+                    break;
+                case 2:
+                    Fun.ThayTheThamSo(1);
+                    break;
+                case 3:
+                    Fun.ThayTheBang(1);
+                    break;
+                default:
+                    Console.WriteLine("Nhập 1 2 3 thôi ngu nó vừa");
+                    break;
+            }
         }
     }
 }
